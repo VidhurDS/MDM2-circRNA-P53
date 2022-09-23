e21929e135a71e51e87a4b4ebc58c0ca089d3d38
Swapna Vidhur Daulatabad <dsvidhur@gmail.com>
2022-09-23 11:06:49 -0400

    Add files via upload

---
+++ Alignment/SRR3228430_download.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=32 
+#PBS -l walltime=10:00:00
+#PBS -l gres=ccm
+#PBS -N download_SRR3228430
+
+module load ccm
+
+cd /N/dc2/projects/CancerFight/Vidhur/fastqs_GSE79249
+
+ccmrun /gpfs/home/s/w/swapdaul/BigRed2/sratoolkit.2.8.2-1-ubuntu64/bin/fastq-dump --split-files --gzip SRR3228430
+
+
